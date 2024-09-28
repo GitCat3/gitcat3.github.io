a3e62a5af8a0b224115309f7d876f83257b6426d
GitCat3 <hulkbuster119@gmail.com>
2024-09-28 10:03:19 -0400

    adding sveltekit rewrite

---
+++ src/lib/components/App.svelte
@@ -0,0 +1,23 @@
+<script>
+  import { Canvas } from '@threlte/core'
+  import Scene from './Scene.svelte'
+</script>
+
+<div class="main-canvas">
+  <Canvas>
+    <Scene />
+  </Canvas>
+</div>
+
+<h1 style="color: red;"> Hello World!</h1>
+
+<style>
+	.main-canvas {
+	  width: 100%;
+	  height: 100%;
+	  position: absolute;
+	  top: 0;
+	  left: 0;
+	  z-index: 1;
+	}
+</style>
---
+++ src/lib/components/Scene.svelte
@@ -0,0 +1,43 @@
+<script>
+  import { T } from '@threlte/core'
+  import { ContactShadows, Float, Grid} from '@threlte/extras'
+</script>
+
+<T.PerspectiveCamera
+  makeDefault
+  position={[-10, 10, 10]}
+  fov={15}
+  on:create={({ ref }) => {
+    ref.lookAt(0, 1, 0)
+  }}
+></T.PerspectiveCamera>
+
+<T.DirectionalLight
+  intensity={0.8}
+  position.x={5}
+  position.y={10}
+/>
+<T.AmbientLight intensity={0.2} />
+
+<Grid
+  position.y={-0.001}
+  cellColor="#ffffff"
+  sectionColor="#ffffff"
+  sectionThickness={0}
+  fadeDistance={25}
+  cellSize={2}
+/>
+
+<ContactShadows
+  scale={10}
+  blur={2}
+  far={2.5}
+  opacity={0.5}
+/>
+
+<Float floatIntensity={0.5} floatingRange={[0,1]}>
+  <T.Mesh position={[0, 1, 0]}>
+    <T.BoxGeometry />
+    <T.MeshStandardMaterial color="#BCFF00"/>
+  </T.Mesh>
+</Float>
